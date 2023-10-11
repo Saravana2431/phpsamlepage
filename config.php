2de324add218552b11c5b709950f08055aa825da
naveen kumar <137175151+orewa-snk01@users.noreply.github.com>
2023-10-11 16:01:25 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "snk-db.cl6bkeffvlim.ap-northeast-2.rds.amazonaws.com";
+$servername = "database-1.cl6bkeffvlim.ap-northeast-2.rds.amazonaws.com";
 $username = "naveen";
 $password = "naveen";
 $dbname = "naveen";
